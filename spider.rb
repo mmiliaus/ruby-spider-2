e5ea693d844b8914536460a4098be701feb8924e
Martynas Miliauskas <mmiliauskass@gmail.com>
2013-09-03 16:15:06 +0100

    Step 4. Scraping Abstract

--- spider.rb
+++ spider.rb
@@ -3,12 +3,18 @@ require 'open-uri'
 WIKIPEDIA_DOMAIN = 'http://en.wikipedia.org'
 
 resource = ARGV[0]
-
 if resource.nil?
   puts %Q(Please provide resource name, i.e.: "Spacex", "V_for_vendetta")
   exit
 end
 
+# get "abstract" part of the Wikipedia page
+def get_abstract source_html
+  m = source_html.match /\<table.+?infobox.+?>.+<\/table>\s+(.+)<div.+?id="toc".*?>/im
+  m[1]
+end
+
 url = "#{WIKIPEDIA_DOMAIN}/wiki/#{resource}"
 source = open(url).read
-puts source
+
+puts get_abstract(source)
