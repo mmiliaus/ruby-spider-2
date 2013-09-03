5cb95fdc68ac45d8f3e6bb8c40c9bacf0dc6872d
Martynas Miliauskas <mmiliauskass@gmail.com>
2013-09-03 16:12:46 +0100

    Step 3. Adding Guard

--- spider.rb
+++ spider.rb
@@ -4,7 +4,11 @@ WIKIPEDIA_DOMAIN = 'http://en.wikipedia.org'
 
 resource = ARGV[0]
 
-url = "#{WIKIPEDIA_DOMAIN}/wiki/#{resource}"
+if resource.nil?
+  puts %Q(Please provide resource name, i.e.: "Spacex", "V_for_vendetta")
+  exit
+end
 
+url = "#{WIKIPEDIA_DOMAIN}/wiki/#{resource}"
 source = open(url).read
 puts source
